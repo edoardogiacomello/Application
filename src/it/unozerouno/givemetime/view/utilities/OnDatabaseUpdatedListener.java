b4a3009633603cc6bb8589f17bbb674144685cbf
Edoardo Giacomello <edoardo.giacomello1990@gmail.com>
2015-02-09 18:58:34 +0100

    Full wipe for Android Studio migration

--- src/it/unozerouno/givemetime/view/utilities/OnDatabaseUpdatedListener.java
+++
@@ -1,8 +0,0 @@
-package it.unozerouno.givemetime.view.utilities;
-
-public abstract class OnDatabaseUpdatedListener<ExpectedResult> {
-	protected abstract void onUpdateFinished(ExpectedResult updatedItem);
-	public void updateFinished(ExpectedResult updatedItem){
-		onUpdateFinished(updatedItem);
-	}
-}
